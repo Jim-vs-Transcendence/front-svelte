25fab3b976bb14bd4f16cf861d0bb6ed595e49e8
Ubuntu <ubuntu@ip-172-31-46-232.ap-northeast-2.compute.internal>
2023-06-01 06:02:24 +0000

    push

--- svelte/src/routes/main/+page.svelte
+++ svelte/src/routes/main/+page.svelte
@@ -1,22 +1,5 @@
 <script lang="ts">
-    import { auth } from '../../service/store';
-    import { onMount } from 'svelte';
-    import { goto } from '$app/navigation'
 
-    let userInfo : any; //여기 any는 나중에 타입 정해지면 바꾸는게 좋을 듯함
-
-    onMount(async () => {
-        try {
-            userInfo = await auth.isLogin();
-            if (userInfo == false)
-                throw("잘못된 접근");
-        }
-        catch(error) {
-            //토큰이 없거나, 유효하지 않다는 것을 의미
-            alert("잘못된 접근");
-            goto('.');
-        }
-    });
 
     const goChat = () => {
         //정보를 보낼때, contextAPI 활용하면 될 듯 함.
